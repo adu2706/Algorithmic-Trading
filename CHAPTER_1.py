fb39f7fcfec7de73410e784d99ac9934efae3617
Adarsh PAndey <adu2706adarsh@gmail.com>
2025-06-05 23:08:46 +0530

    CHAPTER_1.py

--- CHAPTER_1.py
+++ CHAPTER_1.py
@@ -41,3 +41,41 @@ td2 = timedelta(days=3)
 print(f'Time difference: {td2}')
 
 print(f'Addition: {td1} + {td2} = {td1 + td2}')
+print(f'Substraction: {td1} - {td2} = {td1 - td2}')
+print(f'Multiplication: {td1} * 2.5 = {td1 * 2.5}')
+
+td3 = timedelta(hours=50, minutes=59, seconds=60)
+print(f'Time difference: {td3}')
+print(f'Total seconds in 2 days, 3:00:00: {td3.total_seconds()}')
+
+
+#########Recipe 3: Operations on datetime objects
+
+from datetime import date, datetime, timedelta
+
+date_today = date.today()
+print(f"Today's Date: {date_today}")
+
+date_5days_later = date_today + timedelta(days=5)
+print(f'Date 5 days later: {date_5days_later}')
+
+date_5days_ago = date_today - timedelta(days=5)
+print(f'Date 5 days ago: {date_5days_ago}')
+
+print(date_5days_later > date_5days_ago)
+print(date_5days_later < date_5days_ago)
+print(date_5days_later > date_today > date_5days_ago)
+
+current_timestamp = datetime.now()
+time_now = current_timestamp.time()
+print(f'Time now: {time_now}')
+
+time_5minutes_later = (current_timestamp + timedelta(minutes=5)).time()
+print(f'Time 5 minutes later: {time_5minutes_later}')
+
+time_5minutes_ago = (current_timestamp - timedelta(minutes=5)).time()
+print(f'Time 5 minutes ago: {time_5minutes_ago}')
+
+print(time_5minutes_later > time_5minutes_ago)
+print(time_5minutes_later < time_5minutes_ago)
+print(time_5minutes_later > time_now > time_5minutes_ago)
